305a51911ff8419b61bbf5e3e630589fb236fb8e
shallot302 <2257678042@qq.com>
2024-08-16 22:02:40 +0800

    1222

---
+++ obstacle2/源.c
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+///* platinum.c -- your weight in platinum */
+//int main(void)
+//{
+//	float weight; /* 你的体重 */
+//	float value; /* 相等重量的白金价值 */
+//	printf("Are you worth your weight in platinum?\n");
+//	printf("Let's check it out.\n");
+//	printf("Please enter your weight in pounds: ");
+//	/* 获取用户的输入 */
+//	scanf("%f", &weight);
+//	getchar();
+//	getchar();
+//	/* 假设白金的价格是每盎司$1700 */
+//	/* 14.5833用于把英镑常衡盎司转换为金衡盎司[1]*/
+//	value = 1700.0 * weight * 14.5833;
+//	printf("Your weight in platinum is worth $%.2f.\n", value);
+//	printf("You are easily worth that! If platinum prices drop,\n");
+//	printf("eat more to maintain your value.\n");
+//	return 0;
+//}
+
+
+//int main(void) {
+//	printf("%d\n\n", sizeof(unsigned));
+//	printf("%d\n\n", sizeof(signed));
+//	printf("%d\n\n", sizeof(_Bool));
+//	return 0;
+//}
+
+//int main(void) {
+//	char gresd = 'F';
+//	printf("%c", gresd);
+//	return 0;
+//}
+//int ch1;
+//void ch() {
+//	scanf("%c", &ch1);
+//}
+//int main() {
+//	printf("please enter a character.\n\n");
+//	ch();
+//	getchar();
+//	getchar();
+//	printf("the ASCII number for %c is %d", ch1, ch1);
+//	return 0;
+//}
+
+/* altnames.c -- 可移植整数类型名 */
+//#include <inttypes.h> // 支持可移植类型
+//int main(void)
+//{
+//	int32_t me32; // me32是一个32位有符号整型变量
+//	me32 = 45933945;
+//	printf("First, assume int32_t is int: ");
+//	printf("me32 = %d\n", me32);
+//	printf("Next, let's not make any assumptions.\n");
+//	printf("Instead, use a \"macro\" from inttypes.h: ");
+//	printf("me32 = %" PRId32 "\n", me32);
+//	return 0;
+//}
