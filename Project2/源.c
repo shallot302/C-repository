0e417e89cfba2559bd818d7c505a535f8351f928
shallot302 <2257678042@qq.com>
2024-08-16 22:00:38 +0800

    1222343

---
+++ Project2/源.c
@@ -0,0 +1,142 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main(void) {
+//	//	int num;
+//	//	num = 1;
+//	//	printf("I am a simple ");
+//	//	printf("computer\n");
+//	//	printf("My favourite number is %d because it is the first\n", num);
+//	//	getchar();
+//	//  return 0;
+//	//}
+//
+//
+//		//该程序打印一年有多少周
+//
+//	//	int s;
+//	//	s = 56;
+//	//	printf("There are %d weeks in a year.", s);
+//	//	return 0;
+//	//}
+//	/*int num;
+//	num = 2;
+//	printf("%d + %d = %d", num, num, num + num);*/
+//	/*int word = 3020;
+//	int line = 350;
+//	printf("There were %d words and %d lines.", word, line);
+//	return 0;*/
+//
+//}
+//int main(void)
+//{
+//	int a, b;
+//	a = 5;
+//	b = 2; /* 第7行 */
+//	b = a; /* 第8行 */
+//	a = b; /* 第9行 */
+//	printf("%d %d\n", b, a);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	int x, y;
+//	x = 10;
+//	y = 5; /* 第7行 */
+//	y = x + y; /*第8行*/
+//	x = x * y; /*第9行*/
+//	printf("%d %d\n", x, y);
+//	return 0;
+//}
+// 
+// 
+// 
+// QUESTION 4
+//void jolly(void) {
+//
+//	printf("For he's a jolly good fellow!\n");
+//}
+//void deny(void) {
+//
+//	printf("Which nobody can deny!\n");
+//}
+//int main(void) {
+//	jolly();
+//	jolly();
+//	jolly();
+//	deny();
+//	return 0;
+//}
+// 
+// 
+// 
+// 
+// QUESTION 3
+//int main(void) {
+//	int age = 18;
+//	int day;
+//	day = age * 365;
+//	printf("I`m %d years old,about %d days.", age, day);
+//
+//	return 0;
+//}
+// 
+// 
+// 
+// QUESTION 5
+//void br(void) {
+//	printf("Brazil, Russia\n");
+//}
+//void ic(void) {
+//	printf("India, China\n");
+//}
+//int main(void) {
+//	printf("Brazil, Russia, India, China\n");
+//	ic();
+//	br();
+//
+//	return 0;
+//}
+// 
+// 
+// 
+// QUESTION 6
+//int main(void) {
+//	int toes = 10;
+//	int toes2;
+//	int toes3;
+//	toes2 = toes * 2;
+//	toes3 = toes * toes;
+//	printf("toes = %d,toes的两倍 = %d,toes的平方 = %d", toes, toes2, toes3);
+//}
+// 
+// 
+// 
+// 
+// QUESTION 7
+//void smile(void) {
+//	printf("smile!");
+//}
+//int main(void) {
+//	smile(), smile(), smile();
+//	printf("\n\n");
+//	smile(), smile();
+//	printf("\n\n");
+//	smile();
+//	return 0;
+//}
+//void two(void) {
+//	printf("two\n\n");
+//}
+//void one_three(void) {
+//	printf("one\n\n");
+//	two();
+//	printf("three\n\n");
+//}
+//int main(void) {
+//	printf("starting now\n\n");
+//	one_three();
+//	printf("done!");
+//	return 0;
+//}
